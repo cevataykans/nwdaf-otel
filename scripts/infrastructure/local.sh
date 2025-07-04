6939db5598b4da46b6786d27a04c9dec344079fa
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-07-04 16:21:27 +0200

    Add local setup script for otel

---
+++ scripts/infrastructure/local.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+
+# Paths to the directories for the corresponding application (Path ending in the directory)
+# Manually adapt to local setup (TODO: Clone istio and Aether if necessary BUT: Values still need to be adapted manually)
+ISTIO_DIR=/home/sevinc/jungmann/istio
+AETHER_DIR=/home/sevinc/jungmann/core/aether-onramp/
+
+# remove dangling data from disk from older metric installations
+sudo rm -rf /opt/local-path-provisioner/
+sudo mkdir /opt/local-path-provisioner/
+
+cd "$AETHER_DIR"
+make aether-k8s-install
+cd "$current_dir"
+
+sleep 30s
+
+# TODO add if amchine should be integrated in the cluster
+# copy kubeconfig to cmvm7
+#scp -i ~/.ssh/cmvm7_key ~/.kube/config jungmann@cmvm7.cit.tum.de:~/.kube/config
+#ssh -i ~/.ssh/cmvm7_key  jungmann@cmvm7.cit.tum.de "sudo rm -rf /opt/local-path-provisioner/; sudo mkdir /opt/local-path-provisioner/"
+
+# Set the necessary traffic forwarding rules in the machine running the ue simulation. Otherwise, UPF cannot forward data
+#ssh -i ~/.ssh/cmvm7_key  jungmann@cmvm7.cit.tum.de "sudo ip route add 192.168.252.0/24 via 131.159.25.123 dev ens192; sudo ip route add 192.168.250.0/24 via 131.159.25.123 dev ens192"
+#ssh -i ~/.ssh/cmvm7_key  jungmann@cmvm8.cit.tum.de "sudo ip route add 192.168.252.0/24 via 131.159.25.123 dev ens192; sudo ip route add 192.168.250.0/24 via 131.159.25.123 dev ens192"
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
+sleep 1m
+
+#kubectl apply -f otel_operator_go_dec.yaml
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
+sleep 2m
+
+kubectl apply -f jungmann/setup_scripts/collector_filtered_elastic.yaml   # collector_filtered.yaml
+kubectl apply -f jungmann/setup_scripts/jaeger_config_3.yaml       #jaeger_config.yaml
+
+cd "$ISTIO_DIR"
+sh install_istio.sh
+cd "$current_dir"
+
+sleep 1m
+
+cd "$AETHER_DIR"
+make aether-5gc-install
+make aether-amp-install
+#make aether-ueransim-install
+cd "$current_dir"
+
+bash jungmann/setup_scripts/remove_istio_from_met_nf.sh
+bash jungmann/setup_scripts/port-forward.sh &
+#bash ./injection.sh
