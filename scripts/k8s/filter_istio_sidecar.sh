e4bbfd1dcde835552ede2ce87dbc7d39901e4ba7
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-06 11:48:43 +0200

    Remove nwdaf from sidecar filter

--- scripts/k8s/filter_istio_sidecar.sh
+++ scripts/k8s/filter_istio_sidecar.sh
@@ -1,4 +1,3 @@
 kubectl patch deployment webui -n  aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
 kubectl patch deployment metricfunc -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
 kubectl patch deployment simapp -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
-kubectl patch deployment nwdaf-analytics-info -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
