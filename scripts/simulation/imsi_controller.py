258d73a2b0cbfee65bb7a2879f33d036a137d4ca
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-19 14:46:13 +0100

    Fix bugs in imsi creation

--- scripts/simulation/imsi_controller.py
+++ scripts/simulation/imsi_controller.py
@@ -1,4 +1,52 @@
-import re
+import yaml
+
+def edit_imsi_lines(core_values_path, total_requested_ues):
+    initial_device_count = 14
+    imsi_start = 208930100007487
+
+    # Load existing YAML
+    with open(core_values_path, 'r') as f:
+        data = yaml.safe_load(f)
+
+    config = data['omec-sub-provision']['config']['simapp']['cfgFiles']['simapp.yaml']['configuration']
+    subscribers = config['subscribers']
+    subscribers.clear()
+
+    subscribers.append({
+        "ueId-start": f"{imsi_start}",
+        "ueId-end": f"{imsi_start + initial_device_count - 1}",
+        "plmnId": "20893",
+        "opc": "981d464c7c52eb6e5036234984ad0bcf",
+        "op": "",
+        "key": "5122250214c33e723a5dd523fc145fc0",
+        "sequenceNumber": "16f3b3f70fc2"
+    })
+    cur_device_count = initial_device_count
+    if cur_device_count < total_requested_ues:
+        new_imsi_start = imsi_start + cur_device_count
+        remaining_count = total_requested_ues - cur_device_count
+        subscribers.append({
+            "ueId-start": f"{new_imsi_start}",
+            "ueId-end": f"{new_imsi_start + remaining_count}",
+            "plmnId": "20893",
+            "opc": "981d464c7c52eb6e5036234984ad0bcf",
+            "op": "",
+            "key": "5122250214c33e723a5dd523fc145fc0",
+            "sequenceNumber": "16f3b3f70fc2"
+        })
+    config['subscribers'] = subscribers
+
+    available_imsis = []
+    for i in range(0, total_requested_ues):
+        available_imsis.append(f'{imsi_start + i}')
+
+    config['device-groups'][0]['imsis'] = available_imsis
+
+    # Write it back to the YAML file
+    with open(core_values_path, 'w') as f:
+        yaml.dump(data, f, default_flow_style=False, sort_keys=False)
+
+
 
 def filter_ueid_lines(file_path,total_requested_ues):
     try:
@@ -48,5 +96,5 @@ def filter_ueid_lines(file_path,total_requested_ues):
 
 
 file_path = "../aether-onramp/deps/5gc/roles/core/templates/sdcore-5g-values.yaml"
-total_requested_ues=500
-filter_ueid_lines(file_path, total_requested_ues)
+ue_count=500
+edit_imsi_lines(file_path, ue_count)
