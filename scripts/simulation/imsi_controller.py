aeff726c8946b90a4fb00e8422494d2e02afe698
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-19 14:48:39 +0100

    Improve argument passing to imsi creation

--- scripts/simulation/imsi_controller.py
+++ scripts/simulation/imsi_controller.py
@@ -1,4 +1,5 @@
 import yaml
+import sys
 
 def edit_imsi_lines(core_values_path, total_requested_ues):
     initial_device_count = 14
@@ -46,55 +47,65 @@ def edit_imsi_lines(core_values_path, total_requested_ues):
     with open(core_values_path, 'w') as f:
         yaml.dump(data, f, default_flow_style=False, sort_keys=False)
 
+# def filter_ueid_lines(file_path,total_requested_ues):
+#     try:
+#         with open(file_path, 'r') as file:
+#             lines = file.readlines()
+#
+#         # Find all occurrences of "ueId-start: " or "ueId-end: " followed by an integer
+#         pattern = re.compile(r"(ueId-(?:start|end): )\"(\d+)\"")
+#         max_value = None
+#         max_line_index = None
+#         max_match = None
+#
+#         # Search for the max integer in matching lines
+#         for index, line in enumerate(lines):
+#             match = pattern.search(line)
+#             if match:
+#                 #if "ueId-start: " in line or "ueId-end: " in line:
+#                 number = int(match.group(2))
+#                 if max_value is None or number > max_value:
+#                     max_value = number
+#                     max_line_index = index
+#                     max_match = match
+#
+#         if max_value is not None:
+#             new_value = max_value + total_requested_ues
+#
+#             # repalce old end value
+#             lines[max_line_index] = pattern.sub(fr'ueId-end: "{new_value}"', lines[max_line_index], 1)
+#
+#             for i_s, line_s in enumerate(lines):
+#                 if '- "'+str(max_value)+'"' in line_s:
+#                     for iter in range(total_requested_ues):
+#                         lines[i_s]+='                - "'+str(max_value + 1 + iter) +'"\n'
+#
+#             with open(file_path, 'w') as file:
+#                 file.writelines(lines)
+#
+#             print(f"Updated largest ueId ({max_value}) to {new_value} in the file.")
+#         else:
+#             print("No matching 'ueId-start:' or 'ueId-end:' entries found.")
+#
+#
+#     except FileNotFoundError:
+#         print(f"Error: The file '{file_path}' was not found.")
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
 
 
-def filter_ueid_lines(file_path,total_requested_ues):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
+if len(sys.argv) != 2:
+    print('Usage: python3 imsi_controller.py <maximum_ue_count>')
+    sys.exit(1)
 
-        # Find all occurrences of "ueId-start: " or "ueId-end: " followed by an integer
-        pattern = re.compile(r"(ueId-(?:start|end): )\"(\d+)\"")
-        max_value = None
-        max_line_index = None
-        max_match = None
+ue_count = -1
+try:
+    ue_count = int(sys.argv[1])
+except ValueError:
+    print('Error: argument must be an integer')
+    sys.exit(1)
 
-        # Search for the max integer in matching lines
-        for index, line in enumerate(lines):
-            match = pattern.search(line)
-            if match:
-                #if "ueId-start: " in line or "ueId-end: " in line:
-                number = int(match.group(2))
-                if max_value is None or number > max_value:
-                    max_value = number
-                    max_line_index = index
-                    max_match = match
+print(f'Creating imsi for devices up to: {ue_count}')
 
-        if max_value is not None:
-            new_value = max_value + total_requested_ues
-
-            # repalce old end value
-            lines[max_line_index] = pattern.sub(fr'ueId-end: "{new_value}"', lines[max_line_index], 1)
-
-            for i_s, line_s in enumerate(lines):
-                if '- "'+str(max_value)+'"' in line_s:
-                    for iter in range(total_requested_ues):
-                        lines[i_s]+='                - "'+str(max_value + 1 + iter) +'"\n'
-
-            with open(file_path, 'w') as file:
-                file.writelines(lines)
-
-            print(f"Updated largest ueId ({max_value}) to {new_value} in the file.")
-        else:
-            print("No matching 'ueId-start:' or 'ueId-end:' entries found.")
-
-
-    except FileNotFoundError:
-        print(f"Error: The file '{file_path}' was not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-file_path = "../aether-onramp/deps/5gc/roles/core/templates/sdcore-5g-values.yaml"
-ue_count=500
+file_path = '../aether-onramp/deps/5gc/roles/core/templates/sdcore-5g-values.yaml'
 edit_imsi_lines(file_path, ue_count)
