308c3eadd482c60ca38de9d094f7570ec381225c
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-19 16:39:56 +0100

    Mind gnb having imsi

--- scripts/simulation/imsi_controller.py
+++ scripts/simulation/imsi_controller.py
@@ -56,7 +56,7 @@ def edit_imsi_lines(core_values_path, total_requested_ues):
         remaining_count = total_requested_ues - cur_device_count
         subscribers.append({
             "ueId-start": f"{new_imsi_start}",
-            "ueId-end": f"{new_imsi_start + remaining_count - 1}",
+            "ueId-end": f"{new_imsi_start + remaining_count}",
             "plmnId": "20893",
             "opc": "981d464c7c52eb6e5036234984ad0bcf",
             "op": "",
@@ -82,52 +82,6 @@ def edit_imsi_lines(core_values_path, total_requested_ues):
 
     with open(core_values_path, 'w') as f:
         f.write(reverted_text)
-
-# def filter_ueid_lines(file_path,total_requested_ues):
-#     try:
-#         with open(file_path, 'r') as file:
-#             lines = file.readlines()
-#
-#         # Find all occurrences of "ueId-start: " or "ueId-end: " followed by an integer
-#         pattern = re.compile(r"(ueId-(?:start|end): )\"(\d+)\"")
-#         max_value = None
-#         max_line_index = None
-#         max_match = None
-#
-#         # Search for the max integer in matching lines
-#         for index, line in enumerate(lines):
-#             match = pattern.search(line)
-#             if match:
-#                 #if "ueId-start: " in line or "ueId-end: " in line:
-#                 number = int(match.group(2))
-#                 if max_value is None or number > max_value:
-#                     max_value = number
-#                     max_line_index = index
-#                     max_match = match
-#
-#         if max_value is not None:
-#             new_value = max_value + total_requested_ues
-#
-#             # repalce old end value
-#             lines[max_line_index] = pattern.sub(fr'ueId-end: "{new_value}"', lines[max_line_index], 1)
-#
-#             for i_s, line_s in enumerate(lines):
-#                 if '- "'+str(max_value)+'"' in line_s:
-#                     for iter in range(total_requested_ues):
-#                         lines[i_s]+='                - "'+str(max_value + 1 + iter) +'"\n'
-#
-#             with open(file_path, 'w') as file:
-#                 file.writelines(lines)
-#
-#             print(f"Updated largest ueId ({max_value}) to {new_value} in the file.")
-#         else:
-#             print("No matching 'ueId-start:' or 'ueId-end:' entries found.")
-#
-#
-#     except FileNotFoundError:
-#         print(f"Error: The file '{file_path}' was not found.")
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
 
 
 if len(sys.argv) != 2:
