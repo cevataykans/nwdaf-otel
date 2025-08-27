020e0e7f8fc446e047b96ec5cce01ff0f633461d
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-27 17:09:32 +0200

    Also benchmark burst effect

--- scripts/simulation/gnbsim_configs.py
+++ scripts/simulation/gnbsim_configs.py
@@ -22,9 +22,10 @@ def unquote_jinja_vars(yaml_text: str) -> str:
     fixed_text = re.sub(r"''(?!$)", r"'", first_pass, flags=re.MULTILINE)
     return fixed_text
 
-def edit_config(data, start_imsi, ue_count, cur_gnb):
+def edit_config(data, start_imsi, ue_count, cur_gnb, is_parallel):
     pdu_test = data['configuration']['profiles'][1]
     pdu_test['ueCount'] = ue_count
+    pdu_test['execInParallel'] = is_parallel
     pdu_test['startImsi'] = f'{start_imsi + 1}' # assume this gnb takes one imsi
     data['configuration']['profiles'][1] = pdu_test
 
@@ -37,12 +38,12 @@ def edit_config(data, start_imsi, ue_count, cur_gnb):
 def get_config_name(prefix, index):
     return f'{prefix}-{index}.yaml'
 
-def create_gnbsim_custom_configs(folder_path, template_path, prefix, start_imsi, gnb_count, ue_count):
+def create_gnbsim_custom_configs(folder_path, template_path, prefix, start_imsi, gnb_count, ue_count, is_parallel):
     with open(template_path, 'r') as f:
         data = yaml.safe_load(f)
 
     for i in range(gnb_count):
-        edit_config(data, start_imsi, ue_count, i)
+        edit_config(data, start_imsi, ue_count, i, is_parallel)
 
         config_name = get_config_name(prefix, i)
         with open(f'{folder_path}/{config_name}', 'w') as f:
@@ -69,7 +70,7 @@ def edit_gnbsim_main_config(main_config_path, gnb_count, prefix):
     with open(main_config_path, 'w') as f:
         yaml.dump(data, f, default_flow_style=False, sort_keys=False)
 
-def edit_gnbsim_config(aether_path, gnb_count, ue_count_per_gnb):
+def edit_gnbsim_config(aether_path, is_parallel, gnb_count, ue_count_per_gnb):
     imsi_start = 208930100007487
     gnbsim_main_config_path = f'{aether_path}/vars/main.yml'
     gnbsim_gnb_configs_folder = f'{aether_path}/deps/gnbsim/config'
@@ -78,30 +79,39 @@ def edit_gnbsim_config(aether_path, gnb_count, ue_count_per_gnb):
 
     edit_gnbsim_main_config(gnbsim_main_config_path, gnb_count, config_name_prefix)
     create_gnbsim_custom_configs(gnbsim_gnb_configs_folder, gnbsim_template_config_path,
-                                 config_name_prefix, imsi_start, gnb_count, ue_count_per_gnb)
+                                 config_name_prefix, imsi_start, gnb_count, ue_count_per_gnb, is_parallel)
 
 # usage gnb_count, ue_count per gnb
-if len(sys.argv) != 3:
-    print('Usage: python3 gnbsim_configs.py <gnb_count> <ue_count_per_gnb>')
+if len(sys.argv) != 4:
+    print('Usage: python3 gnbsim_configs.py <exec_parallel> <gnb_count> <ue_count_per_gnb>')
+    sys.exit(1)
+
+parallel = False
+try:
+    parallel = bool(sys.argv[1])
+except ValueError:
+    print('Error: argument must be a bool: true, false')
     sys.exit(1)
 
 gnb_count = -1
 try:
-    gnb_count = int(sys.argv[1])
+    gnb_count = int(sys.argv[2])
 except ValueError:
     print('Error: argument must be an integer')
     sys.exit(1)
 
 ue_count = -1
 try:
-    ue_count = int(sys.argv[2])
+    ue_count = int(sys.argv[3])
 except ValueError:
     print('Error: argument must be an integer')
     sys.exit(1)
 
-print(f'Creating {gnb_count} with {ue_count} per gnb...')
+
+
+print(f'Creating {gnb_count} with {ue_count} per gnb... parallel -> {parallel}')
 
 #onramp_path = '../../Thesis/test/aether-onramp'
 onramp_path = '../aether-onramp'
 file_path = onramp_path
-edit_gnbsim_config(file_path, gnb_count, ue_count)
+edit_gnbsim_config(file_path, parallel, gnb_count, ue_count)
