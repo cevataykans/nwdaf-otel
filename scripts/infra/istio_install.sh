afdf486b589e1e31bf21037ebb2d9111e7f90888
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-10-11 21:07:22 +0200

    Fix OTEL and istio deployments

--- scripts/infra/istio_install.sh
+++ scripts/infra/istio_install.sh
@@ -5,5 +5,8 @@ helm repo update
 helm install istio-base istio/base -n istio-system --set defaultRevision=default --create-namespace --version "1.17.8"
 helm install istiod istio/istiod -n istio-system --version "1.17.8" --wait
 
+kubectl create namespace aether-5gc
+kubectl label namespace aether-5gc istio-injection=enabled
+
 # Contains the tracer settings
 kubectl apply -f scripts/istio.yaml
