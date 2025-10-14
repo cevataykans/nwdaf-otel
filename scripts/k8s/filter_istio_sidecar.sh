b34f0693529f01dc1ab50a0f6c5e48e683a7b591
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-10-14 15:01:41 +0200

    Edit collector config

--- scripts/k8s/filter_istio_sidecar.sh
+++ scripts/k8s/filter_istio_sidecar.sh
@@ -1,4 +1,4 @@
-#kubectl patch deployment webui -n  aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
-#kubectl patch deployment metricfunc -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
-#kubectl patch deployment simapp -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
+kubectl patch deployment webui -n  aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
+kubectl patch deployment metricfunc -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
+kubectl patch deployment simapp -n aether-5gc -p '{"spec":{"template":{"metadata":{"annotations":{ "sidecar.istio.io/inject": "false"}}}}}'
 echo "Istio Sidecar injection filters applied"
