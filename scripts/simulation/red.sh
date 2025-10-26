5d73a7241c8a26397bfcae552210de8a2cf2dbe7
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-10-26 14:42:03 +0000

    Debug data collection

--- scripts/simulation/red.sh
+++ scripts/simulation/red.sh
@@ -9,10 +9,10 @@ nwdaf_dir="$HOME/nwdaf-otel/"
 #TODO: create folder with UE count, save every graph under there
 current_day=$(date '+%d-%m-%Y')
 archive_folder="${current_day}-ue-${ue_value}-$(date +%s)"
-mkdir ${archive_folder}
+mkdir "${archive_folder}"
 
 start_ts=$(date +%s)
-echo "Start time:  $start_ts"
+echo "Start time: $start_ts"
 cd $nwdaf_dir
 python3 scripts/simulation/gnbsim_configs.py "True" "1" "$ue_value"
 
@@ -26,7 +26,7 @@ end_ts=$(date +%s)
 kubectl port-forward service/rancher-monitoring-prometheus -n cattle-monitoring-system 9090:9090 &
 prom_pf_process_id=$!
 # Function that can query Prometheus
-archive() {
+query_prom() {
   local query_name=$1
   local archive_name=$2
   local folder=$3
@@ -38,8 +38,8 @@ archive() {
 }
 
 query="histogram_quantile(0.95, sum by(le, span_name) (rate(traces_spanmetrics_latency_bucket{service_name=~\"amf.aether-5gc\"}[30s])))"
-name='latency_bucket'
-archive $query $name $archive_folder
+name="latency_bucket"
+query_prom $query $name $archive_folder
 
 kill $prom_pf_process_id
 echo "Start time:  $start_ts"
