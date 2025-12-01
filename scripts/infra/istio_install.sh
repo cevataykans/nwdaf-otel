b98bc2a58906942da81ff022dea7bec415892e23
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-12-01 21:04:07 +0100

    Revert istio change

--- scripts/infra/istio_install.sh
+++ scripts/infra/istio_install.sh
@@ -2,8 +2,8 @@ helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 
 # Aether k8s version supports up to this version, check compatibility here https://istio.io/latest/docs/releases/supported-releases/
-helm install istio-base istio/base -n istio-system --set defaultRevision=default --create-namespace --version "1.28.0"
-helm install istiod istio/istiod -n istio-system --version "1.28.0" --wait
+helm install istio-base istio/base -n istio-system --set defaultRevision=default --create-namespace --version "1.17.8"
+helm install istiod istio/istiod -n istio-system --version "1.17.8" --wait
 
 kubectl create namespace aether-5gc
 kubectl label namespace aether-5gc istio-injection=enabled
